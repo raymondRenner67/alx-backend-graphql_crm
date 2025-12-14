fed7407804a672c334489ed10278bfb712af2671
Latitude7480 <Latitude7480@DESKTOP-A9HLI2N>
2025-12-14 19:27:25 +0000

    first commit 4

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,8 +1,12 @@
 import graphene
 
 
-class Query(graphene.ObjectType):
+class CRMQuery(graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
+
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
 
 
 schema = graphene.Schema(query=Query)
